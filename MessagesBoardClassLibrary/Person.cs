add1da1835ad609c7240b423343b2f5e9d69b2f0
Miha <Miha@DESKTOP-5T344OH>
2018-12-16 11:00:17 -0800

    Add additional properties and methods

--- MessagesBoardClassLibrary/Person.cs
+++ MessagesBoardClassLibrary/Person.cs
@@ -7,22 +7,65 @@ namespace MessagesBoardClassLibrary
 {
     public class Person
     {
-        //properties
+        // properties
+        public int Id { get; private set;}
         public String FirstName { get; private set; }
         public String LastName { get; private set; }
         public String Email { get; private set; }
         public DateTime BirthDate{ get; private set; }
 
-        //constructor
+        // additional properties we could use in addition to those requested in the requirements
+        public virtual string FullName
+        {
+            get { return $"{FirstName} {LastName}"; }
+        }
 
+        public double Age
+        {
+            get
+            {
+                return (DateTime.Now - this.BirthDate).TotalDays / 365.2425;
+            }
+        }
 
-        //methods
+        // constructor 
+        public Person(int id, string firstName, string lastName, string email, DateTime birthDate)
+            
+        {
+            this.Id = id;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Email = email;
+            this.BirthDate = birthDate;
+        }
+        
+
+        // methods
+        // C - create
         public void CreateAccount()
         {
         }
 
-         
+        // additional methods we could use in addition to those requested in the requirements
 
+        //R - read
+        public void GetAccountInfoById(int Id)
+        {
+            
+        }
+
+        //U- update
+        public void UpdateAccountInfo()
+        {
+
+        }
+         //additional methos we may need
+
+        //D - delete
+        public void RemoveAccount()
+        {
+
+        }
 
     }
 }
