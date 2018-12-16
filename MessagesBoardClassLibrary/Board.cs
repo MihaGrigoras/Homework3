d736e756ee489adaf048a63bac89d82f2afb5496
Miha <Miha@DESKTOP-5T344OH>
2018-12-16 11:26:00 -0800

    Add the Board class

---
+++ MessagesBoardClassLibrary/Board.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MessageBoardClassLibrary
+{
+   public class Board
+    {
+        private IMessageService messageService;
+
+        public Board(IMessageService messageService)
+        {
+            this.messageService = messageService;
+
+        }
+        // C - create
+        public void AddPost(string author, string message, DateTime postDate)
+        {
+            messageService.AddPost(author, message, postDate);
+        }
+
+        // R - read
+        public Post GePostById(int id)
+        {
+            return messageService.GetPostById(id);
+        }
+
+        public List<Post> GetAllPosts()
+        {
+            return messageService.GetAllPosts();
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+       
+
