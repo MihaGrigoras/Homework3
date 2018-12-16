8609af2045fe2a435d6aa19a0dc20957852ff229
Miha <Miha@DESKTOP-5T344OH>
2018-12-16 10:11:47 -0800

    Add the Person class

---
+++ MessagesBoardClassLibrary/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace MessagesBoardClassLibrary
+{
+    public class Person
+    {
+        //properties
+        public String FirstName { get; private set; }
+        public String LastName { get; private set; }
+        public String Email { get; private set; }
+        public DateTime BirthDate{ get; private set; }
+
+        //constructor
+
+
+        //methods
+        public void CreateAccount()
+        {
+        }
+
+         
+
+
+    }
+}
+
