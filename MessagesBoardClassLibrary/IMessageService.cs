86caa4fc78457bea9c60b697c361c8c0945e2dd3
Miha <Miha@DESKTOP-5T344OH>
2018-12-16 11:17:29 -0800

    Add the message service responsible for dealing with messages posted in the board ( this is in addition to the homework requirement)

---
+++ MessagesBoardClassLibrary/IMessageService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MessageBoardClassLibrary
+{
+    public interface IMessageService
+    {
+        Post AddPost(string author, string message, DateTime postedDate);
+
+        Post GetPostById(int id);
+
+        List<Post> GetAllPosts();
+
+        void UpdatePost(int id, string author, string text, DateTime newPostedDate);
+
+        void DeletePost(int id);
+    }
+}
